2a0f9b04503253d3c84acbfaf72fc39ca727a628
otajisan <mtaji@morningcode.io>
2026-01-13 19:36:33 +0900

    initial

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'src/popup/popup.html'),
+        background: resolve(__dirname, 'src/background/background.js'),
+        content: resolve(__dirname, 'src/content/content.js'),
+      },
+      output: {
+        entryFileNames: `[name].js`,
+      },
+    },
+    outDir: 'dist',
+  },
+});
